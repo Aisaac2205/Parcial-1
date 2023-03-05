7b31e76b664f1051733074c0d103be6053ebf5c8
isaacflores2205@gmail.com <isaacflores2205@gmail.com>
2023-03-05 16:06:54 -0600

    Parcial 1.0

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("\nIngrese un número entero positivo mayor que cero:");
+int num = int.Parse(Console.ReadLine());
+
+
+int[] cuadrados = new int[num];
+for (int i = 1; i <= num; i++)
+{
+    cuadrados[i - 1] = i * i;
+}
+
+//Números primos menores o iguales que el numero ingresado "n".
+Console.WriteLine("\nNúmeros primos menores o iguales que "+num,"\n");
+for (int i = 2; i <= num; i++)
+{
+    bool esPrimo = true;
+    for (int j = 2; j < i; j++)
+    {
+        if (i % j == 0)
+        {
+            esPrimo = false;
+            break;
+        }
+    }
+    if (esPrimo)
+    {
+        Console.Write(i + " \n");
+    }
+}
+
+//Multiplos de 4 menores o iguales que el número ingresado "n".
+Console.WriteLine("\nMúltiplos de 4 menores o iguales que " +num,"::\n");
+for (int i = 4; i <= num; i += 4)
+{
+    Console.Write(i + "\n");
+}
+//Incrementa en 3 desde 1 hasta llegar al numero en n.
+Console.WriteLine("\nSecuencia de números que se incrementan en 3:");
+for (int i = 1; i <= num; i += 3)
+{
+    Console.Write(i + "\n");
+}
+//Cuadrados de los numeros menores a n.
+Console.WriteLine("\nCuadrados de los números menores o iguales que "+num,"::\n");
+for (int i = 0; i <= num; i++)
+{
+    Console.WriteLine(cuadrados[i] + " ");
+}
+
+Console.ReadKey();
